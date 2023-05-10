6a0d48b73fca3c65b55230f3619507267241cea8
Sławek <slawek.nonename@gmail.com>
2023-05-10 12:00:06 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 """Demo module."""
+import os
+
 class Main:
     '''Define simple message'''
 
@@ -11,7 +13,8 @@ class Main:
 
     def hello_world(self):
         """Define message function."""
-        self.message = "Hello World"
+        self.message = os.getenv('GITHUB_SHA')
+        
 
 if __name__ == "__main__":
     Main().run()
